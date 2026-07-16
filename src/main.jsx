3bb620d446a07a35c473c6f4a0153a591a53c728
Betül Bircan <betul.bircan@fairteknoloji.com.tr>
2026-07-17 01:10:20 +0300

    Rating yıldız iconu için bağımsız bir StarRaiting componenti oluşturuldu ve propTypesşar eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
+// import StarRaiting from './StarRaiting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    {/* <StarRaiting maxRating={5} color="red"/>
+    <StarRaiting maxRating={10} />
+    <StarRaiting size={64}/> */}
+  </StrictMode>
 )
