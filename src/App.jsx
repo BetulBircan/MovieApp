2bf2ef21bee8a9ca774233beb56cc2b5430d59f9
Betül Bircan <betul.bircan@fairteknoloji.com.tr>
2026-07-20 17:54:11 +0300

    useEffect hooku kullanıldı ve sonsuza kadar istek atması engellendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,4 @@
-import { useState } from "react";
-const movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://image.tmdb.org/t/p/original/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
-  },
-];
+import { useState, useEffect } from "react";
 
 const selected_movie_list = [
   {
@@ -70,11 +40,16 @@ export default function App() {
     const [selectedMovies, setSelectedMovies] = useState(selected_movie_list);
     const query="fight"
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`)
+    useEffect(() => {
+      //First render(mount)
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`)
     .then((res) => res.json())
     .then((data) => {
       setMovies(data.results)
     })
+    },[])
+
+    
   return (
     <>
       <Navbar>
